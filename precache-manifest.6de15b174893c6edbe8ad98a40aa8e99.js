a45a42076067c911cb1328c236b71e191b2a4962
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2021-04-29 18:56:43 +0300

    Updates

---
+++ precache-manifest.6de15b174893c6edbe8ad98a40aa8e99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347e4c08d4b3c2d7acc5f6e6eea6e8e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be4b1df3ed2a05d89f73",
+    "url": "/static/css/main.0480be69.chunk.css"
+  },
+  {
+    "revision": "ec97f736dad091e66079",
+    "url": "/static/js/2.189f2f4e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.189f2f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4b1df3ed2a05d89f73",
+    "url": "/static/js/main.a2dc196f.chunk.js"
+  },
+  {
+    "revision": "8b0f391bfdd48764ca85",
+    "url": "/static/js/runtime-main.15e679b5.js"
+  },
+  {
+    "revision": "6986b7914e2dc2db1a83cb6bbe281a13",
+    "url": "/static/media/ChocoCookie.6986b791.webp"
+  },
+  {
+    "revision": "d5962e31e284b618663b6bfdf7e72d9c",
+    "url": "/static/media/DoubleCookies.d5962e31.webp"
+  }
+]);
