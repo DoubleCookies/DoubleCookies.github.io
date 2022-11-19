093b24122579d9d9759eb48e738c00d8dd9a9a54
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-11-19 13:29:34 +0300

    Github calendar update to latest version

--- src/components/programming/OpenSource/GithubCalendar/GithubCalendar.js
+++ src/components/programming/OpenSource/GithubCalendar/GithubCalendar.js
@@ -2,7 +2,6 @@ import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
 import React from 'react';
 
-
 function GithubCalendar() {
     return (
         <div className="block-medium">
